ad4bd136b58c5c01574c77670ce331f82c02872b
Spodi <spodi2290@aol.com>
2022-12-11 11:50:39 +0100

    Added support for Dec 2022 appinfo format

--- VDFparse.Core/infoFiles/AppInfo.cs
+++ VDFparse.Core/infoFiles/AppInfo.cs
@@ -1,5 +1,41 @@
 namespace VDFparse;
 
+public class AppInfoReaderDec22 : AppInfoReader , IVDFFileReader
+{
+    public new uint Magic { get => 0x07_56_44_28; }
+
+    public new List<Dataset> Read(BinaryReader reader)
+    {
+        var Datasets = new List<Dataset>();
+        while (true)
+        {
+            var id = reader.ReadUInt32();
+            if (id == 0)
+            {
+                return Datasets;
+            }
+
+            Datasets.Add(new AppDatasetDec22
+            {
+                Id = id,
+                Size = reader.ReadUInt32(),
+                InfoState = reader.ReadUInt32(),
+                LastUpdated = DateTimeFromUnixTimestamp(reader.ReadUInt32()),
+                Token = reader.ReadUInt64(),
+                Hash = reader.ReadBytes(20),
+                ChangeNumber = reader.ReadUInt32(),
+                VDFHash = reader.ReadBytes(20),
+                Data = KVParser.Parse(reader),
+            });
+        }
+    }
+    private static DateTime UnixBaseTime { get; } = new DateTime(1970, 1, 1);
+    private static DateTime DateTimeFromUnixTimestamp(uint unixTime)
+    {
+        return UnixBaseTime.AddSeconds(unixTime);
+    }
+}
+
 public class AppInfoReader : IVDFFileReader
 {
     public uint Magic { get => 0x07_56_44_27; }
@@ -28,7 +64,6 @@ public class AppInfoReader : IVDFFileReader
             });
         }
     }
-
     private static DateTime UnixBaseTime { get; } = new DateTime(1970, 1, 1);
     private static DateTime DateTimeFromUnixTimestamp(uint unixTime)
     {
@@ -36,10 +71,14 @@ public class AppInfoReader : IVDFFileReader
     }
 }
 
+public class AppDatasetDec22 : AppDataset
+{
+    public byte[] VDFHash { get; init; } = null!;
+}
+
 public class AppDataset : Dataset
 {
     public uint Size { get; init; }
     public uint InfoState { get; init; }
     public DateTime LastUpdated { get; init; }
-
 }
